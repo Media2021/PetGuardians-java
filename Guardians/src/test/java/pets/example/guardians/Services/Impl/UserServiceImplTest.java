cc11eb986941e45ef4b99cecb5e8f8f232e390fd
Media <95234709+Media2021@users.noreply.github.com>
2023-03-07 18:37:53 +0100

    test2

--- Guardians/src/test/java/pets/example/guardians/Services/Impl/UserServiceImplTest.java
+++ Guardians/src/test/java/pets/example/guardians/Services/Impl/UserServiceImplTest.java
@@ -11,13 +11,16 @@ import pets.example.guardians.Model.User;
 import pets.example.guardians.Model.UserRole;
 import pets.example.guardians.Repository.UserRepo;
 
+
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -108,6 +111,42 @@ class UserServiceImplTest {
 //        Assertions.assertEquals(2, userList.size());
 //    }
 
+    @Test
+    public void testGetAllUsers() {
+
+        UserEntity userEntity1 = new UserEntity();
+        userEntity1.setId(1L);
+        userEntity1.setName("John");
+        userEntity1.setLast_name("Doe");
+        userEntity1.setUser_name("jdoe");
+        userEntity1.setEmail("jdoe@example.com");
+        userEntity1.setAddress("123 Main St");
+        userEntity1.setPassword(123456);
+        userEntity1.setPhone(1234567890);
+        userEntity1.setBirthdate(new Date());
+        userEntity1.setUserRole(UserRole.Admin);
+
+        UserEntity userEntity2 = new UserEntity();
+        userEntity2.setId(2L);
+        userEntity2.setName("Jane");
+        userEntity2.setLast_name("Doe");
+        userEntity2.setUser_name("jane");
+        userEntity2.setEmail("jane@example.com");
+        userEntity2.setAddress("456 Main St");
+        userEntity2.setPassword(789012);
+        userEntity2.setPhone(456789012);
+        userEntity2.setBirthdate(new Date());
+        userEntity2.setUserRole(UserRole.User);
+
+        List<UserEntity> userEntities = Arrays.asList(userEntity1, userEntity2);
+        when(userRepo.findAll()).thenReturn(userEntities);
+
+
+        List<User> users = userServiceImpl.getAllUsers();
+        assertThat(users).hasSize(2);
+        assertThat(users.get(0)).isEqualToComparingFieldByField(new User(1L, "jdoe", "Doe", "John", "jdoe@example.com", "123 Main St", 123456, 1234567890, userEntity1.getBirthdate(), UserRole.Admin));
+        assertThat(users.get(1)).isEqualToComparingFieldByField(new User(2L, "jane", "Doe", "Jane", "jane@example.com", "456 Main St", 789012, 456789012, userEntity2.getBirthdate(), UserRole.User));
+    }
     @Test
     void deleteUser() {
     }
